9b025fb80802fc27e44df2966776123b76911c84
Saúl Laínez <saullainez@hotmail.es>
2021-04-24 08:59:04 -0600

    Helpers
    getGifs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { getGifs } from '../helpers/getGifs';
 import GifGridItem from './GifGridItem';
 
 export const GifGrid = ( { category } ) => {
@@ -6,28 +7,9 @@ export const GifGrid = ( { category } ) => {
     const [images, setImages] = useState([]);
 
     useEffect( () => {
-        getGifs();
-    }, [])
-
-    const getGifs = async() => {
-
-        const url = 'https://api.giphy.com/v1/gifs/search?q=boca juniors&api_key=lV7Z03JbwePPaOSjsvETxUV4icZt5ECd&limit=10';
-        const resp = await fetch( url );
-        const { data } = await resp.json();
-
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        } )
-
-        console.log(gifs);
-        setImages( gifs );
-    }
-
-
+        getGifs( category )
+            .then( setImages )
+    }, [ category ])
 
     return (
         <>
