614f2148eeeb81622161ad8ec8aa175c66750a14
Saúl Laínez <saullainez@hotmail.es>
2021-04-24 09:36:16 -0600

    useFetchGifs
    Obtener imágenes y bandera de carga

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,24 +1,19 @@
 import React from 'react'
 import { useFetchGifs } from '../hooks/useFetchGifs'
-//import { getGifs } from '../helpers/getGifs';
-//import GifGridItem from './GifGridItem';
+import GifGridItem from './GifGridItem';
 
 export const GifGrid = ( { category } ) => {
 
-    /*const [images, setImages] = useState([]);
 
-    useEffect( () => {
-        getGifs( category )
-            .then( setImages )
-    }, [ category ])*/
-
-    const { loading } = useFetchGifs();
+    const { data:images, loading } = useFetchGifs(category);
 
     return (
         <>
             <h3>{ category }</h3>  
-            { loading ? 'Cargando...' : 'data cargada'}
-{/*             <div className="card-grid">
+
+            { loading && <p>Loading</p> }
+
+            <div className="card-grid">
                 {
                     images.map( ( img ) =>
                         <GifGridItem 
@@ -27,7 +22,7 @@ export const GifGrid = ( { category } ) => {
                         />
                     )
                 }
-            </div> */}
+            </div>
         </>
     )
 }
--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,18 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { getGifs } from '../helpers/getGifs';
 
-export const useFetchGifs = () => {
+export const useFetchGifs = (category) => {
 
-    const [state, setstate] = useState({
+    const [state, setState] = useState({
         data: [],
         loading: true
     });
 
-    setTimeout( () => {
-        setstate({
-            data: [1,2],
-            loading: false
-        })
-    }, 3000)
+    useEffect( () => {
+        getGifs( category )
+            .then( imgs => {
+                setState({
+                    data: imgs,
+                    loading:false
+                });
+            } )
+    }, [ category ])
 
     return state;
 
